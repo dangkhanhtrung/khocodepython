8cada3c13468bb86a8f9a43aa099ed135a2c3c8b
dangkhanhtrung <trung.dk070384@gmail.com>
2026-03-28 07:53:30 +0700

    Nộp bài tập 2.3

---
+++ Nguyễn Văn An/2.3/ifelse.py
@@ -0,0 +1,5 @@
+_so = int(input("So nguyen duong: "))
+if _so % 2 == 0:
+    print("So chan")
+else:
+    print("So le")
---
+++ Nguyễn Văn An/2.3/viducactoantu.py
@@ -0,0 +1,8 @@
+a = 46
+b = 10
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a << 3)
+print(a >> 4)
